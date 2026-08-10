766c0f3e4d84fa04a029e880b5e7d8adb7b6c97b
xenoninja <xenoninjaio@gmail.com>
2026-08-10 17:03:54 +0800

    solve ex1.05

---
+++ chapter-01/exercise-1.05.rkt
@@ -0,0 +1,13 @@
+#lang sicp
+
+(define (p) (p))
+(define (test x y)
+        (if (= x 0) 0 y))
+
+(test 0 (p))
+
+; In applicative-order evaluation (which Scheme currently uses), 
+; the (p) is endlessly applied by the first defined rule, which causes a loop.
+; However, in normal-order evaluation,
+; (test 0 (p)) => (if (= 0 0) 0 (p)) => 0.
+; There will be no loop.
